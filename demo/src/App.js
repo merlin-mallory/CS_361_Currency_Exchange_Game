37de4332fd35b7b67c248fd7fa78eeb708364833
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-01-30 20:23:11 +0900

    Added signup button, got rid of ESLint

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// import styled from "styled-components"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="App-header">
         <h1>Currency Exchange Game</h1>
-        
+        <h3>Learn how to play in less than 5 minutes!</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test
         </p>
         <a
           className="App-link"
@@ -19,12 +29,29 @@ function App() {
           Learn React
         </a>
       </header>
-      <footer className="App-footer">
+  )
+}
+
+function Body() {
+  return(
+    <div>
+      <button type="button" onClick={clickMe}>Sign Up!</button>
+    </div>
+      
+  )
+}
+
+function clickMe() {
+  alert('You clicked me!');
+};
+
+function Footer() {
+  return (
+    <footer className="App-footer">
         Made with <img src={logo} className="App-logo" alt="logo" />
         © Merlin Mallory, 2023
       </footer>
-    </div>
-  );
+  )
 }
 
 export default App;

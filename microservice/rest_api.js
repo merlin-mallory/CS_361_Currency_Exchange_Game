64d71208abcc8f5a6854109ef1003a7d9d5118cb
Daniel Hong <dh2716@columbia.edu>
2023-02-13 11:19:20 -0500

    Added numGenerator

--- microservice/rest_api.js
+++ microservice/rest_api.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 
-async function numGenerator() {
-    
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Generates random floats for each currency
+async function numGenerator(currencies) {
+    for (let currency in currencies) {
+        let randomNum = Math.random() * (0.02 - (-0.02)) + (-0.02);
+        currencies[currency] = randomNum
+    }
+    return currencies
 }
 
+// Handles incoming GET requests. 
+// Calls the numGenerator function, passing in the JSON object in the request 
+// Returns randomized floats in JSON 
 app.get("/rates", (req, res) => {
-
+    numGenerator(req.body)
+        .then(currencies => {
+            res.status(201).json(currencies)
+        })
 });
 
 const PORT = process.env.PORT || 3000;

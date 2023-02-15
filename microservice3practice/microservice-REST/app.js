e4cb6885a7f4bf07924e294828582837b1d73396
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-02-15 19:04:00 +0900

    Completed microservice test3

---
+++ microservice3practice/microservice-REST/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json())
+
+// Generates 5 unique random numbers in the range [1, 10]
+function randomNumGenerator() {
+    const randNumSet = new Set()
+    while (randNumSet.size < 5){
+        randNumSet.add(Math.floor(Math.random() * 10) +1)
+    }
+    console.log("Here's the list of unique random numbers between 1 and 10:")
+    console.log([...randNumSet])
+    return randNumSet
+}
+
+// Constructing a new object containing only the selected anime
+async function pickAnime(receivedAnime) {
+    console.log("Here's what we got:", receivedAnime)
+    console.log("inside async")
+    let pickedAnimeArr = []
+    let pickIndexSet = new randomNumGenerator()
+    // pickedAnimeArr.push(receivedAnime[0])
+    for (let pickedIndex of pickIndexSet) {
+        console.log("Picked index:", pickedIndex)
+        pickedAnimeArr.push(receivedAnime[pickedIndex-1])
+        console.log("Arr this iteration:", pickedAnimeArr)
+    }
+    console.log("final arr:", pickedAnimeArr)
+    return pickedAnimeArr
+}
+
+app.get("/pick", (req, res) => {
+    console.log("Pick is called!")
+    pickAnime(req.body)
+        .then(pickedAnimeArr => {
+            res.status(201).json(pickedAnimeArr)
+        })
+})
+
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
---
+++ microservice3practice/microservice-REST/test.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch"
+
+const example = [
+    {title: "One Piece", visual: "Visual1"},
+    {title: "Naruto", visual: "Visual2"},
+    {title: "Death Note", visual: "Visual3"},
+    {title: "Demon Slayer", visual: "Visual4"},
+    {title: "Bleach", visual: "Visual5"},
+    {title: "Code Geass", visual: "Visual6"},
+    {title: "Spy X Family", visual: "Visual7"},
+    {title: "Pokemon", visual: "Visual8"},
+    {title: "Sword Art Online", visual: "Visual9"},
+    {title: "Hunter X Hunter", visual: "Visual10"}
+]
+
+fetch('http://localhost:3000/rates', {
+    method: 'GET',
+    body: JSON.stringify(example),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+    .then(response => response.text())
+    .then(text => console.log(text))
